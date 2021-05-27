fca65d91006ec7b1a8ef99ea7d2d1b213bc5ed1e
MattIPv4 <me@mattcowley.co.uk>
2021-05-27 13:15:10 +0100

    Load NVM

--- zsh-auto-nvm-use.plugin.zsh
+++ zsh-auto-nvm-use.plugin.zsh
@@ -1,11 +1,16 @@
 autoload -U add-zsh-hook
+
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
+
 load-nvmrc() {
   if [[ -f .nvmrc && -r .nvmrc ]]; then
     nvm use
   elif [[ $(nvm version) != $(nvm version default)  ]]; then
-    echo "Reverting to nvm default version"
     nvm use default
   fi
 }
+
 add-zsh-hook chpwd load-nvmrc
 load-nvmrc
