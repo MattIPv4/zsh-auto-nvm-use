729b0724204d1c27e0ab3204fa823ee39f684db3
MattIPv4 <me@mattcowley.co.uk>
2022-08-22 12:56:42 +0100

    Use --no-use flag when loading NVM

--- zsh-auto-nvm-use.plugin.zsh
+++ zsh-auto-nvm-use.plugin.zsh
@@ -1,7 +1,7 @@
 autoload -U add-zsh-hook
 
 export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" --no-use  # This loads nvm (without automatically using a version)
 [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
 load-nvmrc() {
